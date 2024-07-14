1bd6d5b1910273d0246c003d8d10d447d579f2fa
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-07-14 10:35:53 +0200

    Fix: update

--- app/Http/Requests/Admin/UpdateRestaurantRequest.php
+++ app/Http/Requests/Admin/UpdateRestaurantRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Admin;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
-class UdateRestaurantRequest extends FormRequest
+class UpdateRestaurantRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
