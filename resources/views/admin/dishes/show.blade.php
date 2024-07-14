79301a13f33a4edd55edd29c02ce2efa8de6b8ef
WalterCorsini <corsini.walter@gmail.com>
2024-07-14 18:50:30 +0200

    add modify button in show view

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -1,31 +1,36 @@
 @extends('layouts.admin')
 
 @section('content')
-        <h1 class="text-center mt-5">Dettagli Piatto</h1>
-        <div class="form-container p-5">
-            <dt>
-                Nome del Piatto:
-            </dt>
-            <dd>
-                {{$dish->name}}
-            </dd>
-            <dt>
-                Descrizione:
-            </dt>
-            <dd>
-                {{$dish->description}}
-            </dd>
-            <dt>
-                Prezzo:
-            </dt>
-            <dd>
-                {{$dish->price}}
-            </dd>
-            <dt>
-                Image:
-            </dt>
-            <dd>
-                <img src="{{ asset('storage/' . $dish->image) }}" alt="">
-            </dd>
-        </div>
+    <h1 class="text-center mt-5">Dettagli Piatto</h1>
+    <div class="form-container p-5">
+        <dt>
+            Nome del Piatto:
+        </dt>
+        <dd>
+            {{ $dish->name }}
+        </dd>
+        <dt>
+            Descrizione:
+        </dt>
+        <dd>
+            {{ $dish->description }}
+        </dd>
+        <dt>
+            Prezzo:
+        </dt>
+        <dd>
+            {{ $dish->price }}
+        </dd>
+        <dt>
+            Image:
+        </dt>
+        <dd>
+            <img src="{{ asset('storage/' . $dish->image) }}" alt="">
+        </dd>
+
+        <a class="text-black btn btn-warning" href="{{ route('admin.dishes.edit', ['dish' => $dish->slug]) }}">
+            <i class="fa-solid fa-pen"></i>
+        </a>
+
+    </div>
 @endsection
