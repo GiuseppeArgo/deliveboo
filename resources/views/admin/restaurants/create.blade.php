3db7fecaf2566c598927a3e7d78b0360667e494a
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-07-14 13:11:00 +0200

    Fix: orror's message in create.blade

--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -4,48 +4,79 @@
     <div class="container">
         <h1>Crea Il tuo Ristorante</h1>
 
-        @include('partials.errors')
+
 
         <form action="{{ route('admin.restaurants.store') }}" method="POST" enctype="multipart/form-data">
 
             @csrf
 
             <div class="mb-3">
-                <label for="name" class="form-label">Nome Ristorante</label>
-                <input value="{{ old('name') }}" type="text" name="name" class="form-control" id="name"
-                    aria-describedby="name_restaurant">
+                <label for="name" class="form-label">Nome Ristorante:
+                    {{-- error message --}}
+                    @error('name')
+                        <span class="text-danger"> {{ $errors->first('name') }} </span>
+                    @enderror
+                    {{-- /error message --}}
+                </label>
+                <input value="{{ old('name') }}" type="text" name="name"
+                    class="form-control
+                 {{-- dynamic class with red border --}}
+                @error('name') is-invalid @enderror"
+                    {{-- /dynamic class with red border --}} id="name" aria-describedby="name_restaurant">
             </div>
 
             <div class="mb-3">
-                <label for="address" class="form-label">Indirizzo</label>
-                <input value="{{ old('address') }}" type="text" name="address" class="form-control" id="address"
-                    aria-describedby="address">
+                <label for="address" class="form-label">Indirizzo :
+                    {{-- error message --}}
+                    @error('address')
+                        <span class="text-danger"> {{ $errors->first('address') }} </span>
+                    @enderror
+                    {{-- /error message --}}
+                </label>
+                <input value="{{ old('address') }}" type="text" name="address"
+                    class="form-control
+                @error('address') is-invalid @enderror
+                "
+                    id="address" aria-describedby="address">
             </div>
-
+            
+            {{-- Immaggine --}}
             <div class="mb-3">
-                <label for="image" class="form-label">Poster</label>
-                <input value="image" type="file" name="image" class="form-control" id="image"
-                    aria-describedby="image">
+                <label for="image" class="form-label"> Immagine</label>
+                <input @error('image') is-invalid @enderror value="{{ old('image') }}" type="file" name="image"
+                    class="form-control" id="image" aria-describedby="image">
             </div>
-
+            {{-- /Immagine --}}
+           
+            {{-- Descrizione --}}
             <div class="mb-3">
-                <label for="description" class="form-label">Descrizione</label>
-                <textarea for="description" class="form-control" name="description" id="description" rows="3">
+                <label for="description" class="form-label">Descrizione :
+                    {{-- error message --}}
+                    @error('description')
+                        <span class="text-danger"> {{ $errors->first('description') }} </span>
+                    @enderror
+                    {{-- /error message --}}
+                </label>
+                <textarea for="description @error('description') is-invalid @enderror"
+                    class="form-control @error('description') is-invalid @enderror" name="description" id="description" rows="3">
                         {{ old('description') }}
             </textarea>
             </div>
-
+            {{-- Descrizione --}}
+            
             {{-- Tipologia --}}
             <p>Tipologie:</p>
+            @error('tipologies')
+                <span class="text-danger"> {{ $errors->first('tipologies') }} </span>
+            @enderror
             <div class="container mb-4">
                 <div class="row" role="group" aria-label="Basic checkbox toggle button group">
 
                     @foreach ($listTypes as $curType)
                         <div class="col-4 btn-group flex flex-wrap mt-3">
-                            @if (old('tipologies') !== null)
+                            @if ('tipologies' !== null)
                                 <input type="checkbox" class="btn-check" id="tech-{{ $curType->id }}" name="tipologies[]"
                                     value="{{ $curType->id }}" @checked(in_array($curType->id, old('tipologies', [])))>
-                           
                             @endif
 
                             <label class="btn btn-outline-primary"
@@ -57,8 +88,14 @@
             {{-- Tipologia --}}
 
             <div class="mb-3">
-                <label for="p_iva" class="form-label">Partita Iva</label>
-                <input value="{{ old('address') }}" type="number" name="p_iva" class="form-control" id="p_iva"
+                <label for="p_iva" class="form-label">Partita Iva: 
+                    {{-- error message --}}
+                    @error('p_iva')
+                        <span class="text-danger"> {{ $errors->first('p_iva') }} </span>
+                    @enderror
+                    {{-- /error message --}}
+                </label>
+                <input value="{{ old('address') }}" type="number" name="p_iva" class="form-control  @error('p_iva') is-invalid @enderror" id="p_iva"
                     aria-describedby="p_iva">
             </div>
 
