9cc2e4e9e1f5c602fe86d8289bc66138a286f5b8
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-07-16 17:21:41 +0200

    add api index controller

---
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    public function index(Request $request)
+    {
+        $restaurants = Restaurant::with('types')->get();
+
+        // se arriva il parametro type filtra per tipo
+        if ($request->type) {
+            $typeId = $request->type;
+            $restaurants = Restaurant::with('types')
+                ->whereHas('types', function ($query) use ($typeId) {
+                    $query->where('id', $typeId); // Assicurati che il nome della colonna corrisponda a quello della tua tabella di collegamento
+                })->get();
+            dd($restaurants);
+        }
+
+        $data = [
+            'result' => $restaurants
+        ];
+        return response()->json($data);
+    }
+}
