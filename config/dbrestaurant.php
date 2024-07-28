eab82719c6edfe4255e8a203df3d30e2d719e33d
WalterCorsini <corsini.walter@gmail.com>
2024-07-28 16:36:30 +0200

    fix responsive

--- config/dbrestaurant.php
+++ config/dbrestaurant.php
@@ -2,83 +2,83 @@
 return [
     [
         'user_id' => 1,
-        'name' => 'La Pergola',
+        'name' => 'Mc Mannie',
         'city' => 'Milano',
         'address' => 'Via Roma 1',
-        'image' => 'img/ristoranti/001.png',
-        'description' => 'Ristorante gourmet con vista panoramica sulla città.',
+        'image' => 'img/001.png',
+        'description' => 'Paninoteca storica in stile Americano con vista panoramica sulla città.',
         'p_iva' => '12345678901'
     ],
     [
         'user_id' => 2,
-        'name' => 'Il Girasole',
+        'name' => 'Sushi Zen',
         'city' => 'Milano',
         'address' => 'Via Milano 2',
         'image' => 'img/ristoranti/002.png',
-        'description' => 'Autentica cucina italiana in un ambiente accogliente.',
+        'description' => 'Scopri la vera cucina Giapponese secondo tradizione.',
         'p_iva' => '12345678902'
     ],
     [
         'user_id' => 3,
-        'name' => 'Trattoria da Mario',
+        'name' => 'Maharaja Ristorante',
         'city' => 'Milano',
         'address' => 'Via Verdi 3',
         'image' => 'img/ristoranti/003.png',
-        'description' => 'Piatti tradizionali e sapori genuini.',
+        'description' => 'Cucina tipica Indiana sia con ricette a base di carne che ricette vegetariane.',
         'p_iva' => '12345678903'
     ],
     [
         'user_id' => 4,
-        'name' => 'Sushi Zen',
+        'name' => 'Mexal',
         'city' => 'Milano',
         'address' => 'Via Giotto 4',
         'image' => 'img/ristoranti/004.png',
-        'description' => 'Sushi e specialità giapponesi preparati con ingredienti freschi.',
+        'description' => 'Nel nostro ristorante ogni boccone è un viaggio nella ricchezza e varietà della tradizione culinaria messicana.',
         'p_iva' => '12345678904'
     ],
     [
         'user_id' => 5,
-        'name' => 'Pizza Pazza',
+        'name' => 'Banh Mi',
         'city' => 'Milano',
         'address' => 'Via Dante 5',
-        'image' => 'img/pizzerie/001.png',
-        'description' => 'Pizzeria con forno a legna e ingredienti di alta qualità.',
+        'image' => 'img/ristoranti/013.png',
+        'description' => 'Ristorante dove ogni dettaglio è progettato per trasformare ogni pasto in un\'esperienza unica.',
         'p_iva' => '12345678905'
     ],
     [
         'user_id' => 6,
-        'name' => 'La Dolce Vita',
+        'name' => 'Takami',
         'city' => 'Milano',
         'address' => 'Via Manzoni 6',
         'image' => 'img/ristoranti/006.png',
-        'description' => 'Cucina italiana contemporanea con un tocco di creatività.',
+        'description' => 'Un Viaggio Culinario nel Cuore della Cultura Cinese.',
         'p_iva' => '12345678906'
     ],
     [
         'user_id' => 7,
-        'name' => 'Ristorante Al Mare',
+        'name' => 'Cocotte Milano',
         'city' => 'Milano',
         'address' => 'Via Marina 7',
         'image' => 'img/ristoranti/007.png',
-        'description' => 'Specialità di pesce fresco e frutti di mare.',
+        'description' => 'Un Viaggio Sensuale nel Cuore della Cucina Francese.',
         'p_iva' => '12345678907'
     ],
     [
         'user_id' => 8,
-        'name' => 'La Bottega del Gusto',
+        'name' => 'Elleniko',
         'city' => 'Milano',
         'address' => 'Via della Spiga 8',
-        'image' => 'img/pizzerie/003.png',
-        'description' => 'Piatti regionali e prodotti locali.',
+        'image' => 'img/ristoranti/015.png',
+        'description' => 'Un Viaggio Sensoriale nel Cuore della Grecia.',
         'p_iva' => '12345678908'
     ],
     [
         'user_id' => 9,
-        'name' => 'Ristorante Il Vecchio Mulino',
+        'name' => 'Hanedan',
         'city' => 'Milano',
         'address' => 'Via Molino 9',
         'image' => 'img/ristoranti/008.png',
-        'description' => 'Cucina rustica in un ambiente storico e suggestivo.',
+        'description' => 'Un Viaggio Sensoriale nel Cuore della Cucina Turca.',
         'p_iva' => '12345678909'
     ],
 ];
--- database/seeders/OrdersTableSeeder.php
+++ database/seeders/OrdersTableSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Order;
+use App\Models\DishOrder;
+use App\Models\Dish;
 use Carbon\Carbon;
 
 class OrdersTableSeeder extends Seeder
@@ -19,45 +21,37 @@ class OrdersTableSeeder extends Seeder
             $newOrder->phone_number = $order['phone_number'];
             $newOrder->email = $order['email'];
             $newOrder->address = $order['address'];
-            $newOrder->total_price = $order['total_price'];
+            $newOrder->total_price = 0; // Inizializza il totale a zero
 
             // Genera una data casuale tra il 1 e il 31 di luglio
-            $randomDayOfMonth = rand(1, 31);
-            $randomDate = Carbon::create(2024, 1, $randomDayOfMonth)->startOfDay();
+            $randomDayOfMonth = rand(1, 30);
+            $randomDate = Carbon::create(2023, 6, $randomDayOfMonth)->startOfDay();
 
             // Imposta le date create_at e updated_at
             $newOrder->created_at = $randomDate;
             $newOrder->updated_at = $randomDate;
 
             $newOrder->save();
-            $rndNumber = rand(1,9);
-            for ($i = 0; $i < rand(1, 3); $i++) {
-                $dishOrder = new \App\Models\DishOrder();
+
+            // Calcola il totale dell'ordine
+            $totalPrice = 0;
+            for ($i = 0; $i <= 1; $i++) {
+                $dishOrder = new DishOrder();
                 $dishOrder->order_id = $newOrder->id;
-
-                if($rndNumber === 1){
-                    $dishOrder->dish_id = rand(1, 5);
-                } else if ($rndNumber === 2){
-                    $dishOrder->dish_id = rand(6, 10);
-                } else if ($rndNumber === 3){
-                    $dishOrder->dish_id = rand(11, 15);
-                } else if ($rndNumber === 4){
-                    $dishOrder->dish_id = rand(16, 20);
-                } else if ($rndNumber === 5){
-                    $dishOrder->dish_id = rand(21, 25);
-                } else if ($rndNumber === 6){
-                    $dishOrder->dish_id = rand(26, 30);
-                } else if ($rndNumber === 7){
-                    $dishOrder->dish_id = rand(31, 35);
-                } else if ($rndNumber === 8){
-                    $dishOrder->dish_id = rand(36, 40);
-                } else if ($rndNumber === 9){
-                    $dishOrder->dish_id = rand(40, 45);
-                }
+                $dishOrder->dish_id = 6 + $i;
                 $dishOrder->quantity = rand(1, 3);
-
                 $dishOrder->save();
+
+                // Recupera il prezzo del piatto dalla tabella Dishes
+                $dishPrice = Dish::find($dishOrder->dish_id)->price;
+
+                // Calcola il costo totale del piatto nell'ordine
+                $totalPrice += $dishPrice * $dishOrder->quantity;
             }
+
+            // Aggiorna il totale dell'ordine
+            $newOrder->total_price = $totalPrice;
+            $newOrder->save();
         }
     }
 }
